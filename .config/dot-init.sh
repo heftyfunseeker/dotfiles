6e1a755c1100f548094504ed94b28bc4dabeb261
heftyfunseeker <nicco.simone@hey.com>
2022-03-15 22:49:22 -0700

    ln -s everything

--- .config/dot-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "linking NvChad custom config"
-rm -r ~/.config/nvim/lua/custom
-ln -s ~/.config/nvchad/custom ~/.config/nvim/lua/custom
---
+++ dot-init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "linking NvChad custom config"
+ln -sf ~/dotfiles/.config/nvchad/custom ~/.config/nvim/lua/custom
+
+echo "linking alacritty"
+ln -sf ~/dotfiles/.config/alacritty ~/.config/alacritty
+
+echo "linking fish"
+ln -sf ~/dotfiles/.config/fish ~/.config/fish
