76e49f69d29bd4f7e4999a68af71060f1e54c64c
Nicco Simone <nicco.simone@hulu.com>
2022-05-17 20:50:42 -0700

    move neovim config and fix init script

--- dot-init.sh
+++ dot-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "linking NvChad custom config"
-ln -snfv ~/dotfiles/.config/nvchad/custom ~/.config/nvim/lua/custom
+echo "linking neovim"
+ln -snfv ~/dotfiles/.config/nvim ~/.config/nvim
 
 echo "linking alacritty"
 ln -snfv ~/dotfiles/.config/alacritty ~/.config/alacritty
