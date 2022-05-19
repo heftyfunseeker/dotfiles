9111e35e8e36c773d521b5cd1bb5db13f742c19f
Nicco Simone <nicco.simone@disneystreaming.com>
2022-05-19 12:52:38 -0700

    packer init

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,10 @@
-return require('packer').startup(function()
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+end
+
+return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'nvim-lua/plenary.nvim'
   use 'kyazdani42/nvim-web-devicons'
@@ -131,4 +137,7 @@ return require('packer').startup(function()
     end
   }
 
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
