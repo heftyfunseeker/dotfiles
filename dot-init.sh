30e5c74c8b6fa596e2adf5bfd5fbeaffc6e1cc89
Nicco Simone <nicco.simone@hulu.com>
2022-05-20 13:56:40 -0700

    fzf & starship configs

--- dot-init.sh
+++ dot-init.sh
@@ -6,5 +6,14 @@ ln -snfv ~/dotfiles/.config/nvim ~/.config/nvim
 echo "linking alacritty"
 ln -snfv ~/dotfiles/.config/alacritty ~/.config/alacritty
 
+echo "linking starship"
+ln -snfv ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+
 echo "linking fish"
 ln -snfv ~/dotfiles/.config/fish ~/.config/fish
+
+# echo "installing brews"
+# bundle brew
+#
+# echo "install fzf keybindings"
+# /usr/local/opt/fzf/install
