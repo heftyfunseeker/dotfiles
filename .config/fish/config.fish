4eb68de888470565cf2a7e490c2ad592ae877112
Nicco Simone <nicco.simone@hulu.com>
2022-06-02 14:13:45 -0700

    fix: linux homebrew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,15 +3,22 @@ if status is-interactive
 end
 
 set PATH $PATH $HOME/.local/bin
+
 set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH $HOME/.cargo/env
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# lsp servers
 set PATH $PATH $HOME/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/bin
 set PATH $PATH $HOME/.local/share/nvim/lsp_servers/rust_analyzer/
 set PATH $PATH $HOME/.local/share/nvim/lsp_servers/eslint/node_modules/vscode-langservers-extracted
 set PATH $PATH $HOME/.local/share/nvim/lsp_servers/eslint/node_modules/vscode-langservers-extracted/bin
 set PATH $PATH $HOME/.local/share/nvim/lsp_servers/eslint/node_modules/vscode-langservers-extracted/lib
 set PATH $PATH $HOME/.local/share/nvim/lsp_servers/tsserver/node_modules/typescript-language-server
+
+# homebrew for linux
+if test uname = "linux"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+end
 
 starship init fish | source
 
